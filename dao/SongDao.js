c08de181159a7fbb4796af4b81384fe80b9d0608
DmitryBelkevich <harleydav@mail.ru>
2026-08-25 22:48:51 +0300

    Update SongDao.js

--- dao/SongDao.js
+++ dao/SongDao.js
@@ -43,7 +43,7 @@ export default class SongDao {
     song.text = result.text;
     song.score = result.score;
     song.playback = result.playback;
-    song.voices = result.voices;
+    // song.voices = result.voices;
     song.instruments = result.instruments;console.log(song);
     
     // return song;
