4fa056b680b721d2bad861a29290a2a7a1f128f4
DmitryBelkevich <harleydav@mail.ru>
2026-08-26 02:25:24 +0300

    Update SongView.js

--- views/SongView.js
+++ views/SongView.js
@@ -82,8 +82,11 @@ export default class SongView {
 
     this.#song.instruments.forEach((instrument, index) => {
       const element_div = document.createElement('div');
+      
       const str = this.getTitle(instrument.tuning) + " [" + instrument.tuning + "]";
       element_div.textContent = str;
+      element.style.color = this.getColor(instrument.tuning);
+      
       element.append(element_div);
     });
     
