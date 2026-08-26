8d595609a0cbbacafe3116c0e8ab244492955958
DmitryBelkevich <harleydav@mail.ru>
2026-08-26 07:21:06 +0300

    Update SongService.js

--- services/SongService.js
+++ services/SongService.js
@@ -6,6 +6,19 @@ export default class SongService {
     
     const song = await songDao.getById(id);
 
+    song.instruments.forEach((instrument, index) => {
+      if (!instrument.tuning) {
+        if (instrument.title == "Guitar")
+          instrument.tuning = ["E", "A", "D", "G", "B", "E"];
+        
+        if (instrument.title == "Bass Guitar")
+          instrument.tuning = ["E", "A", "D", "G"];
+      }
+
+      if (!instrument.capo)
+        instrument.capo = 0;
+    });
+
     console.log(song);
     // fill fields
     
