cfb9a9cb0fd2fd894f784a2bbc5ec4f9434d3635
DmitryBelkevich <harleydav@mail.ru>
2026-08-26 06:44:02 +0300

    Update SongService.js

--- services/SongService.js
+++ services/SongService.js
@@ -1,3 +1,14 @@
+import SongDao from '../../../dao/SongDao.js';
+import Song from '../models/Song.js';
+
 export default class SongService {
-  
+  #songDao = new SongDao();
+
+  async getById(id) {
+    const song = await this.#songDao.getById(id);
+    
+    // fill fields
+    
+    return song;
+  }
 }
