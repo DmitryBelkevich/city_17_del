7406e4d33b1f7f775f52f6b9acf5e4454fdb4cbb
DmitryBelkevich <harleydav@mail.ru>
2026-09-01 19:40:44 +0300

    Update SongView.js

--- views/SongView.js
+++ views/SongView.js
@@ -1,53 +1,12 @@
-export default class SongView {
-  #song;
-  
-  set song(song) {
-    this.#song = song;
-  }
-  
+export default class SongView { 
   create() {
     this.setDisplay();
-    this.setTitle();
-    this.setTabs();
     this.setSettings();
     this.setTuning();
-    this.setText();
   }
 
   setDisplay() {
     const element = document.getElementById("display");
-  }
-
-  setTitle() {
-    const element = document.getElementById("title");
-    
-    const element_h1 = document.createElement('h1');
-    element_h1.textContent = this.#song.title;
-    element.append(element_h1);
-    
-    const element_h2 = document.createElement('h2');
-    element_h2.textContent = this.#song.band;
-    element.append(element_h2);
-  }
-
-  setTabs() {
-    document.getElementById("tab_text").addEventListener("click", () => {
-      console.log("open text tab");
-    });
-    
-    document.getElementById("tab_score").addEventListener("click", () => {
-      if (!this.#song.score)
-        return;
-      
-      window.open(this.#song.score, "_blank");
-    });
-    
-    document.getElementById("tab_playback").addEventListener("click", () => {
-      if (!this.#song.playback)
-        return;
-      
-      window.open(this.#song.playback, "_blank");
-    });
   }
 
   setSettings() {
@@ -90,35 +49,5 @@ export default class SongView {
         console.log("set transposition: " + instrument.transposition);
       }
     });
-  }
-  
-  async setText() {
-    // Fetch the external HTML file
-    const data = await this.loadData(this.#song.text);
-    
-    const element = document.getElementById("text");
-    element.innerHTML = data;
-  }
-  
-  async loadData(path) {
-    const storage = "https://dmitrybelkevich.github.io/city_17/storage/Text & Chords/";
-    
-    try {
-      // 1. Wait for the server headers and response status
-      const response = await fetch(storage + path);
-      
-      // 2. Check if the HTTP status code is successful (200-299)
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      
-      // 3. Wait for the full body data to download and parse as text
-      const data = await response.text();
-      
-      return data;
-    } catch (error) {
-      // 4. Handle network errors or parsing issues
-      console.error('Fetch failed:', error);
-    }
   }
 }
