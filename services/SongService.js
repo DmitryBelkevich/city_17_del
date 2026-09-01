d2ab2161e19e1cd904c24af69fc710d2e0f44222
DmitryBelkevich <harleydav@mail.ru>
2026-09-01 16:27:10 +0300

    Update SongService.js

--- services/SongService.js
+++ services/SongService.js
@@ -1,42 +1,4 @@
-import SongDao from '../dao/SongDao.js';
-
 export default class SongService {
-  #songDao = new SongDao();
-  
-  async getById(id) {
-    const song = await this.#songDao.getById(id);
-
-    song.instruments.forEach((instrument, index) => {
-      if (instrument.title == "Guitar")
-        if (!instrument.tuning)
-          instrument.tuning = ["E", "A", "D", "G", "B", "E"];
-
-      if (instrument.title == "Bass Guitar")
-        if (!instrument.tuning)
-          instrument.tuning = ["E", "A", "D", "G"];
-
-      if (instrument.title == "5-string Bass Guitar")
-        if (!instrument.tuning)
-          instrument.tuning = ["B", "E", "A", "D", "G"];
-
-      if (instrument.title != "Keyboards")
-        if (!instrument.capo)
-          instrument.capo = 0;
-
-      if (instrument.title == "Keyboards")
-        if (!instrument.transposition)
-          instrument.transposition = 0;
-    });
-    
-    return song;
-  }
-
-  getFullInstrumentTitle(tuning) {
-    var title;
-
-    return title;
-  }
-
   isStandardTuning(tuning) {
     const isEqual = (a, b) => a.length === b.length && a.every((val, i) => val === b[i]);
     
